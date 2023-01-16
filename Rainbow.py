cb242305fe8851f151e153e17fca2c59ab8baac9
Florian Werner <f.werner@outlook.de>
2023-01-16 12:01:39 +0100

    Created solution file in jupyter notebook. Added point class and removing functionality.

--- Rainbow.py
+++ Rainbow.py
@@ -2,20 +2,32 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 
-from geometry import ORIGIN, Drawing, Circle, Line, Label, UP, RIGHT, Group
+from geometry import*
 
 mpl.rcParams["font.size"] = 14
+Point.radius = 0.025
 
 A = np.array((np.cos(np.pi/3), np.sin(np.pi/3)))
 
 d = Drawing()
 circle = Circle(ORIGIN, 1)
+point_A = Point(A)
 line = Line(ORIGIN, A, ls="--", color="grey")
 yaxis = Line(-UP*1.5, UP*1.5, ls="-", color="black")
 xaxis = Line(-RIGHT*1.5, RIGHT*1.5, ls="-", color="black")
 label_phi = Label(r"$\varphi_A$", ORIGIN+0.1*UP+0.1*RIGHT)
+label_A = Label('A', A+0.1*UP+0.1*LEFT)
 label_xaxis = Label("x", RIGHT*1.5+0.1*UP)
 label_yaxis = Label("y", UP*1.5+0.1*RIGHT)
 coord_axes = Group(xaxis, yaxis, label_yaxis, label_xaxis)
-d.add(circle, line, label_phi, coord_axes)
-plt.show()
+d.add(circle, line, label_phi, coord_axes, point_A, label_A)
+
+alpha = np.pi/6
+S = np.array((np.cos(alpha), np.sin(alpha))) + A
+point_S = Point(S)
+d.add(point_S)
+# d.remove(point_S)
+d.show()
+
+d.remove(point_S)
+d.show()
--- geometry/geometry.py
+++ geometry/geometry.py
@@ -1,4 +1,5 @@
 import numpy as np
+from IPython.core.display_functions import display
 from matplotlib import pyplot as plt, patches as mpatch
 
 ORIGIN = np.array((0, 0))
@@ -20,6 +21,12 @@ class Drawing:
         for shape in shapes:
             shape.add(self.ax)
 
+    def show(self):
+        return display(self.figure)
+
+    def remove(self, *shapes):
+        for shape in shapes:
+            shape.remove()
 
 class Circle:
     def __init__(self, center, radius):
@@ -27,7 +34,20 @@ class Circle:
         self.radius = radius
 
     def add(self, ax):
-        ax.add_patch(mpatch.Circle(self.center, radius=self.radius, fill=False))
+        self.mpl_patch = mpatch.Circle(self.center, radius=self.radius, fill=False)
+        ax.add_patch(self.mpl_patch)
+    def remove(self):
+        self.mpl_patch.remove()
+
+class Point(Circle):
+    radius = 0.025
+    def __init__(self, center):
+        super(Point, self).__init__(center, Point.radius)
+    def add(self, ax):
+        self.mpl_patch = mpatch.Circle(self.center, radius=Point.radius, fill=True, color='k')
+        ax.add_patch(self.mpl_patch)
+    def remove(self):
+        self.mpl_patch.remove()
 
 
 class Line:
@@ -56,3 +76,4 @@ class Group:
     def add(self, ax):
         for shape in self.shapes:
             shape.add(ax)
+
